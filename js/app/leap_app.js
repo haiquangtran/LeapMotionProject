beb14cf5975a1cce526045ed3f111fcc38c37396
Michael Winton <wintonmr@gmail.com>
2015-07-30 23:17:52 +1200

    Base UI implemented. Tidied up the Leap Image class.

--- js/app/leap_app.js
+++ js/app/leap_app.js
@@ -5,6 +5,9 @@ var App = function() {
   var listImages = [];
   var selectedImages = [];
 
+  /* Helper variables - set in uiInit() */
+  var page, pageContent, sidebar;
+
   /* Initialisation Leap Motion Code */
   var leapInit = function() {
 
@@ -15,21 +18,12 @@ var App = function() {
         var palmPosition = hand.palmPosition;
         var velocity = hand.palmVelocity;
         var direction = hand.direction;
-        var handPosition = hand.screenPosition()
-        console.log(handPosition);
+        var handPosition = hand.screenPosition();
         var xValue = parseInt(handPosition[0], 10) + "px";
         var yValue = parseInt(handPosition[1], 10) + "px";
-
-        console.log(xValue)
-        console.log(yValue);
         $("#cursor").css("top", xValue);
-        $("#cursor").css("right", yValue);
+        $("#cursor").css("left", yValue);
       }
-      // frame.hands.forEach(function(hand, index) {
-      //
-      //
-      //
-      // });
 
     }).use('screenPosition', {scale :0.25});
 
@@ -43,15 +37,15 @@ var App = function() {
   var imagesInit = function() {
 
     // add all images
-    var imageLength = 5;
+    var imageLength = 4;
     var index = 0;
 
     for (; index < imageLength; index++) {
 
       /** instantiate the new image **/
       var image = new LeapImage();
-      //add the image to the list-panel
-      image.addToPanel("list-panel");
+      //add the image to the
+      image.addToDisplay("THUMBNAIL");
       //add the image to the array of list images
       listImages.push(image);
 
@@ -59,10 +53,41 @@ var App = function() {
 
   };
 
+  /* Initialisation UI Code */
+  var uiInit = function() {
+
+    // Set variables - Cache some often used jQuery objects in variables */
+    page            = $('#page-container');
+    pageContent     = $('#page-content');
+    sidebar         = $('#sidebar');
+
+    // Resize #page-content to fill empty space if exists (also add it to resize and orientationchange events)
+    resizePageContent();
+    $(window).resize(function(){ resizePageContent(); });
+    $(window).bind('orientationchange', resizePageContent);
+
+  };
+
+  /* Gets window width cross browser */
+  var getWindowWidth = function(){
+    return window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+  };
+
+  /* Resize #page-content to fill empty space if exists */
+  var resizePageContent = function() {
+      
+    var windowH         = $(window).height();
+    pageContent.css('min-height', windowH + 'px');
+
+  };
+
   return {
     init: function() {
+      uiInit();
       imagesInit(); // Initialise UI Code
-      leapInit(); // Initialise UI Code
+      //leapInit(); // Initialise LeapMotion Code
     }
   };
 }();
--- js/app/leap_image.js
+++ js/app/leap_image.js
@@ -1,26 +1,62 @@
 // class LeapImage
 function LeapImage() {
 
-  // helper vars
-  this.imageDir = "img/";
-  this.imageFormat = ".jpg";
-  this.random_int = Math.floor(Math.random() * 28) + 1;
+    // helper vars
+    var imageDir = "img/";
+    var imageFormat = ".jpg";
+    var random_int = Math.floor(Math.random() * 28) + 1;
 
-  // path to randomly generated image
-  this.src_path = this.imageDir + this.random_int + this.imageFormat;
+    // path to randomly generated image
+    var src_path = imageDir + random_int + imageFormat;
 
-  // create new image
-  this.IMAGE = new Image(190, 170);
+    // create new image
+    this.IMAGE = new Image(190, 170);
 
-  //set the image source to the path we created
-  this.IMAGE.src = this.src_path;
+    //set the image source to the path we created
+    this.IMAGE.src = src_path;
+
+    //constants
+    this.IMAGE_TYPE = {
+        THUMBNAIL: {
+            imageClass: 'image-thumbnail',
+            imageDestId: 'image-thumbnail-display'
+        },
+        MAIN: {
+            imageClass: 'image',
+            imageDestId: 'image-display'
+        },
+    };
 
 };
 
 
 //Add the image to the the panel specified
-LeapImage.prototype.addToPanel = function (panel) {
+LeapImage.prototype.addToDisplay = function (displayType) {
 
-  document.getElementById(panel).appendChild(this.IMAGE);
+    var foundType = null;
+
+    for (type in this.IMAGE_TYPE) {
+
+        if (type === displayType) {
+            foundType = type;
+            break;
+        }
+
+    }
+
+    if (foundType != null) {
+
+        var imageOptions = this.IMAGE_TYPE[foundType];
+        var imageClass = imageOptions.imageClass;
+        var imageDestId = imageOptions.imageDestId;
+
+        $(this.IMAGE).addClass(imageClass);
+        $(this.IMAGE).addClass("selected");
+
+        document.getElementById(imageDestId).appendChild(this.IMAGE);
+
+    } else {
+        console.log("Unknown image type: " + displayType);
+    }
 
 }
