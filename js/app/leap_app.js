af5054018f17b007bb05c90df6c785aee93df42d
Michael Winton <wintonmr@gmail.com>
2015-07-31 17:38:09 +1200

    Updating with select logic, move logic and scale logic.

--- js/app/leap_app.js
+++ js/app/leap_app.js
@@ -1,96 +1,125 @@
-var App = function() {
+var App = function () {
 
-  // Global vars
-  var mainImages = [];
-  var listImages = [];
-  var selectedImages = [];
+    // Global vars
+    var mainImages = [];
+    var listImages = [];
+    var selectedImages = [];
 
-  /* Helper variables - set in uiInit() */
-  var page, pageContent, sidebar;
+    /* Helper variables - set in uiInit() */
+    var page, pageContent, sidebar, imageDisplay;
 
-  /* Initialisation Leap Motion Code */
-  var leapInit = function() {
+    /* Initialisation Leap Motion Code */
+    var leapInit = function () {
 
-    Leap.loop(function(frame) {
+        Leap.loop(function (frame) {
 
-      if (frame.hands.length > 0) {
-        var hand = frame.hands[0];
-        var palmPosition = hand.palmPosition;
-        var velocity = hand.palmVelocity;
-        var direction = hand.direction;
-        var handPosition = hand.screenPosition();
-        var xValue = parseInt(handPosition[0], 10) + "px";
-        var yValue = parseInt(handPosition[1], 10) + "px";
-        $("#cursor").css("top", xValue);
-        $("#cursor").css("left", yValue);
-      }
+            if (frame.hands.length > 0) {
+                var hand = frame.hands[0];
+                var palmPosition = hand.palmPosition;
+                var velocity = hand.palmVelocity;
+                var direction = hand.direction;
+                var handPosition = hand.screenPosition();
+                var xValue = parseInt(handPosition[0], 10) + "px";
+                var yValue = parseInt(handPosition[1], 10) + "px";
+                $("#cursor").css("top", xValue);
+                $("#cursor").css("left", yValue);
+            }
 
-    }).use('screenPosition', {scale :0.25});
+        }).use('screenPosition', {
+            scale: 0.25
+        });
 
-    // This is fairly important - it prevents the framerate from dropping while there are no hands in the frame.
-    // Should probably default to true in LeapJS.
-    Leap.loopController.loopWhileDisconnected = true;
+        // This is fairly important - it prevents the framerate from dropping while there are no hands in the frame.
+        // Should probably default to true in LeapJS.
+        Leap.loopController.loopWhileDisconnected = true;
 
-  };
+    };
 
-  /* Initialisation Images Code */
-  var imagesInit = function() {
+    /* Initialisation Images Code */
+    var imagesInit = function () {
 
-    // add all images
-    var imageLength = 4;
-    var index = 0;
+        // add all images
+        var imageLength = 4;
+        var index = 0;
 
-    for (; index < imageLength; index++) {
+        for (; index < imageLength; index++) {
 
-      /** instantiate the new image **/
-      var image = new LeapImage();
-      //add the image to the
-      image.addToDisplay("THUMBNAIL");
-      //add the image to the array of list images
-      listImages.push(image);
+            /** instantiate the new image **/
+            var image = new LeapImage();
+            //add the image to the
+            image.addToDisplay("THUMBNAIL");
+            //add the image to the array of list images
+            listImages.push(image);
 
-    }
+        }
 
-  };
+    };
 
-  /* Initialisation UI Code */
-  var uiInit = function() {
+    /* Initialisation UI Code */
+    var uiInit = function () {
 
-    // Set variables - Cache some often used jQuery objects in variables */
-    page            = $('#page-container');
-    pageContent     = $('#page-content');
-    sidebar         = $('#sidebar');
+        // Set variables - Cache some often used jQuery objects in variables */
+        page            = $('#page-container');
+        pageContent     = $('#page-content');
+        sidebar         = $('#sidebar');
+        imageDisplay    = $('#image-display');
 
-    // Resize #page-content to fill empty space if exists (also add it to resize and orientationchange events)
-    resizePageContent();
-    $(window).resize(function(){ resizePageContent(); });
-    $(window).bind('orientationchange', resizePageContent);
+        // Resize #page-content to fill empty space if exists (also add it to resize and orientationchange events)
+        resizePageContent();
+        $(window).resize(function () {
+            resizePageContent();
+        });
+        
+        $(window).bind('orientationchange', resizePageContent);
 
-  };
+    };
+    
+    /* Initialisation Test Code */
+    var testInit = function () {
 
-  /* Gets window width cross browser */
-  var getWindowWidth = function(){
-    return window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
-  };
+        // add all images
+        var imageLength = 4;
+        var index = 0;
 
-  /* Resize #page-content to fill empty space if exists */
-  var resizePageContent = function() {
-      
-    var windowH         = $(window).height();
-    pageContent.css('min-height', windowH + 'px');
+        for (; index < imageLength; index++) {
 
-  };
+            /** instantiate the new image **/
+            var image = new LeapImage();
+            //add the image to the
+            image.addToDisplay("MAIN");
+            //add the image to the array of list images
+            mainImages.push(image);
 
-  return {
-    init: function() {
-      uiInit();
-      imagesInit(); // Initialise UI Code
-      //leapInit(); // Initialise LeapMotion Code
-    }
-  };
+        }
+
+    };
+
+    /* Gets window width cross browser */
+    var getWindowWidth = function () {
+        return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    };
+
+    /* Resize #page-content to fill empty space if exists */
+    var resizePageContent = function () {
+
+        var windowH = $(window).height();
+        pageContent.css('min-height', windowH + 'px');
+        sidebar.css('min-height', windowH + 'px');
+        imageDisplay.css('min-height', windowH-30 + 'px');
+
+    };
+
+    return {
+        init: function () {
+            uiInit();       // Initialise UI Code
+            imagesInit();   // Initialise Images Code
+            //testInit();     // Initialise Test Code
+            //leapInit();     // Initialise LeapMotion Code
+        }
+    };
 }();
 
 /* Initialise app when page loads */
-$(function(){ App.init(); });
+$(function () {
+    App.init();
+});
--- js/app/leap_image.js
+++ js/app/leap_image.js
@@ -12,17 +12,29 @@ function LeapImage() {
     // create new image
     this.IMAGE = new Image(190, 170);
 
-    //set the image source to the path we created
+    // set the image source to the path we created
     this.IMAGE.src = src_path;
-
-    //constants
+    
+    // set the z-index
+    $(this.IMAGE).css('z-index', 0);
+    
+    // set temporary bound values
+    this.minBoundX = 0;
+    this.minBoundY = 0
+    this.maxBoundX = 500;
+    this.maxBoundY = 500;
+    
+    // images last modified timestamp
+    this.last_modified = Date.now();
+    
+    //constants    
     this.IMAGE_TYPE = {
         THUMBNAIL: {
             imageClass: 'image-thumbnail',
             imageDestId: 'image-thumbnail-display'
         },
         MAIN: {
-            imageClass: 'image',
+            imageClass: 'image movable',
             imageDestId: 'image-display'
         },
     };
@@ -30,7 +42,7 @@ function LeapImage() {
 };
 
 
-//Add the image to the the panel specified
+// Add the image given the type specified
 LeapImage.prototype.addToDisplay = function (displayType) {
 
     var foundType = null;
@@ -50,13 +62,148 @@ LeapImage.prototype.addToDisplay = function (displayType) {
         var imageClass = imageOptions.imageClass;
         var imageDestId = imageOptions.imageDestId;
 
+        // remove the element before we add it
+        $(this.IMAGE).remove();
+        
         $(this.IMAGE).addClass(imageClass);
         $(this.IMAGE).addClass("selected");
 
+        // add it
         document.getElementById(imageDestId).appendChild(this.IMAGE);
 
     } else {
         console.log("Unknown image type: " + displayType);
     }
 
+}
+
+LeapImage.prototype.tap = function () {
+    
+    if( this._isSelected() ){
+        $(this.IMAGE).removeClass("selected");
+    } else {
+        $(this.IMAGE).addClass("selected")
+    }
+    
+}
+
+LeapImage.prototype.moveTo = function (x, y) {
+    
+    this._calculateBounds();
+    var coords = this._coords();
+    
+    this.IMAGE.style.left = Math.min(this.maxBoundX, Math.max(this.minBoundX, x)) + "px";
+    this.IMAGE.style.top = Math.min(this.maxBoundY, Math.max(this.minBoundY, y)) + "px";
+    
+}
+
+
+LeapImage.prototype.scaleUp = function () {
+    this._scale(1.1)
+}
+
+LeapImage.prototype.scaleDown = function () {
+    this._scale(0.9)
+}
+
+LeapImage.prototype.bringForward = function(){
+    //TODO
+}
+
+LeapImage.prototype.bringBackward = function(){
+    //TODO
+}
+
+LeapImage.prototype.isInBounds = function (x, y) {
+    
+    var coords = this._coords();
+    
+    var top = coords.top,
+        bottom = coords.top + coords.height,
+        left = coords.left,
+        right = coords.left + coords.width;
+    
+    var satisfiesVerticalBounds = y > top && bottom > y;
+    var satisfiesHorizontalBounds = x > left && right > x;
+    
+    return satisfiesHorizontalBounds && satisfiesVerticalBounds;
+    
+}
+
+// Helper methods
+
+LeapImage.prototype._isSelected = function () {
+    return $(this.IMAGE).hasClass("selected");
+}
+
+LeapImage.prototype._isInMainDisplay = function () {
+    var mainImageClass = this.IMAGE_TYPE["MAIN"].imageClass;
+    return $(this.IMAGE).hasClass(mainImageClass);
+}
+
+LeapImage.prototype._scale = function (scale) {
+    
+    var width = $(this.IMAGE).css("width").replace(/[^-\d\.]/g, ''),
+        height = $(this.IMAGE).css("height").replace(/[^-\d\.]/g, ''),
+        newWidth = Math.floor(width * scale),
+        newHeight = Math.floor(height * scale);
+    
+    $(this.IMAGE).css("width", newWidth);
+    $(this.IMAGE).css("height", newHeight);
+      
+}
+
+LeapImage.prototype._updateZIndex = function (modZ) {
+    var curZ = $(this.IMAGE).css('z-index').replace(/[^-\d\.]/g, ''),
+        newZ = curZ + modZ;
+    
+    $(this.IMAGE).css('z', newZ);
+}
+
+LeapImage.prototype._coords = function () {
+    
+    var result = new Object();
+    
+    result.left = $(this.IMAGE).offset().left;
+    result.top = $(this.IMAGE).offset().top;
+    result.width = this.IMAGE.offsetWidth;
+    result.height = this.IMAGE.offsetHeight;
+    
+    return result;
+    
+}
+
+LeapImage.prototype._calculateBounds = function () {
+    
+    var parentElement = this._isInMainDisplay ? this.IMAGE_TYPE["MAIN"].imageDestId : this.IMAGE_TYPE["THUMBNAIL"].imageDestId,
+        parentElement = $('#'+parentElement);
+    
+    var parentOffset = parentElement.offset();
+    
+    var parentOffsetTop = parentOffset.top,
+        parentOffsetLeft = parentOffset.left,
+        parentOffsetWidth = parentElement.outerWidth(),
+        parentOffsetHeight = parentElement.outerHeight();
+    
+    var thisOffsetWidth = this.IMAGE.offsetWidth,
+        thisOffsetHeight = this.IMAGE.offsetHeight;
+    
+    this.minBoundX = parentOffsetLeft;
+    this.minBoundY = parentOffsetTop;
+    this.maxBoundX = parentOffsetLeft + parentOffsetWidth - thisOffsetWidth;
+    this.maxBoundY = parentOffsetTop + parentOffsetHeight - thisOffsetHeight;
+    
+    // for testing purposes
+    this._setHelperBoxPos();
+    
+}
+
+LeapImage.prototype._setHelperBoxPos = function () {
+    var minBox = document.getElementById('min');
+    minBox.style.left = this.minBoundX + 'px';
+    minBox.style.top = this.minBoundY + 'px';
+    
+    var maxBox = document.getElementById('max');
+    maxBox.style.left = this.maxBoundX + 'px';
+    maxBox.style.top = this.maxBoundY + 'px';
 }
